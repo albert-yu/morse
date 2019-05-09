040e85fe8200b81872742a2793d3f1efad6ea4b1
Albert Yu <albert.yu@gmail.com>
2019-05-09 16:46:29 -0500

    Add functions to header file

--- include/imapcmd.h
+++ include/imapcmd.h
@@ -17,10 +17,28 @@ char* imapcmd_select_box(char *box_name);
 
 
 /*
+ * Creates a folder.
+ */
+char *imapcmd_create_folder(char *foldername);
+
+
+/*
+ * Fetches the subject of the message WITHOUT opening it (marking
+ * as read)
+ */
+char* imapcmd_uid_get_subject(char *uid);
+
+
+/*
  * Fetches the text of the message with the given UID
  */
 char* imapcmd_uid_fetch_body_text(char *uid);
 
+
+/*
+ * Lists the folders that are available.
+ */
+char* imapcmd_list_boxes();
 
 #ifdef __cplusplus
 }
--- src/imapcmd.c
+++ src/imapcmd.c
@@ -5,6 +5,7 @@
 
 #define DEFAULT_IMAP_CMD_LENGTH 64
 
+
 /*
  * Prepends "SELECT " to a given box name.
  * Is this better with strncat?   
@@ -13,6 +14,17 @@ char* imapcmd_select_box(char *box_name) {
     const char *template = "SELECT %s";
     char *buffer = calloc(DEFAULT_IMAP_CMD_LENGTH, sizeof(*buffer));
     sprintf(buffer, template, box_name);
+    return buffer;
+}
+
+
+/*
+ * Creates a folder
+ */
+char *imapcmd_create_folder(char *foldername) {
+    const char *template = "CREATE %s";
+    char *buf = calloc(DEFAULT_IMAP_CMD_LENGTH, sizeof(*buf));
+    sprintf(buf, template, foldername);
     return buffer;
 }
 
