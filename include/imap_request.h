27f2a7a09500e4f8dc1f3e7c0b408b650627e09f
Albert Yu <albert.yu@gmail.com>
2019-05-10 17:21:10 -0500

    Create IMAP request DTO

---
+++ include/imap_request.h
@@ -0,0 +1,17 @@
+#ifndef IMAP_REQUEST_H
+#define IMAP_REQUEST_H
+
+
+typedef struct imap_request_t {
+    /* the IMAP command to execute */
+    char *command;
+    
+    /* function that executes the command
+     * and returns a status code. 
+     * the arg is the IMAP command
+     */
+    int (*send_func)(char*);
+} ImapRequest;
+
+
+#endif
