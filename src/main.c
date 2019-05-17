cdebc5063e1cc951f2f1ba5d5b86bab6b000c93e
Albert Yu <albert.yu@gmail.com>
2019-05-17 08:41:52 -0500

    Restructure main()

--- src/main.c
+++ src/main.c
@@ -6,29 +6,28 @@
 
 
 int main(int argc, char *argv[]) {  
-
-    // list_last_n("[Gmail]/All Mail", 1);
-    // list_last_n("INBOX", 10);
-    // morse_list_folders();
     MorseClient *client = morse_client_login(MailProvider_Google);
     char *email = NULL;
     if (argc == 2) {
         email = argv[1];
     }
-    else {
-        email = "test@foo.com";
-    }
-    if (client) {
-        SmtpRequest request;
 
-        request.to = email;
-        request.cc = NULL;
-        request.bcc = NULL;
-        request.subject = "Test email";
-        request.body = "This is a test email.";
-        request.mimetype = "text/plain";
-        request.attachments = NULL;
-        morse_client_sendmail(client, &request);
+    if (client) {
+        if (email) {
+            printf("Sending to: %s\n", email);
+            SmtpRequest request;
+            request.to = email;
+            request.cc = NULL;
+            request.bcc = NULL;
+            request.subject = "Test email";
+            request.body = "This is a test email.";
+            request.mimetype = "text/plain";
+            request.attachments = NULL;
+            morse_client_sendmail(client, &request);
+        }
+        else {
+            printf("Receiving email.\n");
+        }
         morse_client_logout(client);
     }
     return 0;
